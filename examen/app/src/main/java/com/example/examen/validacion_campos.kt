0f820c2ad033633ce7ad5e61fdb68607b4412f11
Rafael Minaya <r.minaya.cajia@gmail.com>
2025-11-17 18:49:57 -0500

    Final

---
+++ examen/app/src/main/java/com/example/examen/validacion_campos.kt
@@ -0,0 +1,20 @@
+package com.example.examen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class validacion_campos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_validacion_campos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
