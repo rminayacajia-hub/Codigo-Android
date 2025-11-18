c1393ee99656a0be58ac09fef5a96e2a636b2ccc
Rafael Minaya <r.minaya.cajia@gmail.com>
2025-11-17 22:28:40 -0500

    accion-insertar-examen

--- examen/app/src/main/java/com/example/examen/menuprincipal.kt
+++ examen/app/src/main/java/com/example/examen/menuprincipal.kt
@@ -7,7 +7,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class menuprincipal : AppCompatActivity() {
-    // variable para la conexion a mysql con php var url="http://10.0.2.2:8080/WSAGENDA26/datos/persona.php"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
